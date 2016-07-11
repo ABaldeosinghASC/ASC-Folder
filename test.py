eb2d1d80218d14649172f06e7188776cc5c323cd
ABaldeosinghASC <anthony.baldeosingh@gmail.com>
2016-07-11 14:27:05 -0400

    First Commit
    Test

---
+++ test.py
@@ -0,0 +1,13 @@
+from Myro import *
+init("sim") #Start simukator
+forward(3,1)
+backward (3,2)
+turnBy(-90, "deg") #=> 
+turnBy(-33, "deg") #=>
+forward (3,3)
+turnBy(90, "deg") 
+forward(1,2)
+turnBy(75, "deg")
+forward(3,8)
+turnBy(155, "deg")
+forward(3,20)
