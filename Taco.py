dce3befb44ce1bb32a65d7f9e47cf07af0709de0
ABaldeosinghASC <anthony.baldeosingh@gmail.com>
2016-07-15 09:17:14 -0400

    robot roaming

---
+++ Taco.py
@@ -0,0 +1,17 @@
+from Myro import *
+init("com3) #Start simulator
+forward(1,4) 
+turnBy(90, "deg")
+forward(1,4)
+turnBy(90, "deg")
+forward(1,4)
+turnBy(90, "deg")
+forward(1,4)
+a = 3
+forward(1, a)
+turnBy(90, "deg")
+forward(1, a)
+turnBy(90, "deg")
+forward(1, a)
+turnBy(90, "deg")
+forward(1, a)
